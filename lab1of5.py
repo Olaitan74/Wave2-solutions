ea4f73872d1948365ee2c0730daf5b28849b5c3c
Olaitan <55138894+Olaitan74@users.noreply.github.com>
2020-08-03 20:15:15 +0100

    Corrected my code
    I added 'is_noble_gas' to my dictionary

--- lab1of5.py
+++ lab1of5.py
@@ -1,5 +1,5 @@
-elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
-            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H', 'is_noble_gas': 'False'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He', 'is_noble_gas': 'True'}}
 
 # todo: Add an 'is_noble_gas' entry to the hydrogen and helium dictionaries
 # hint: helium is a noble gas, hydrogen isn't
